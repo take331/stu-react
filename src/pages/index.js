2745cbf23f5910d9f7a37646cb92a23b46008c46
take331 <t.takeru0331@icloud.com>
2024-03-09 20:27:31 +0900

    ♻️ HeaderとLogoのコンポーネント化

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Links } from "@/components/Links";
+import { Header } from "@/components/Header";
+import { Logo } from "@/components/Logo";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,40 +18,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+        <Header />
+        <Logo />
         <Links />
       </main>
     </>
